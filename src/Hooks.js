6f725937204e57a85478e8a0366ae4808c7ec42a
Benoit <b.mignault@gmail.com>
2022-03-14 18:33:41 -0400

    reajustement Hooks

--- src/Hooks.js
+++ src/Hooks.js
@@ -17,6 +17,7 @@ export default function Hooks(props) {
   // Déclare une nouvelle variable d'état, qu’on va appeler « count »
   // « déstructuration positionnelle » la maniere de nommé la variable et sa modification []
   const [count, setCount] = useState(0);
+  const [old_count, setOldCount] = useState(0);
 
   // Que renvoie useState ? Elle renvoie une paire de valeurs : l’état actuel et une fonction pour le modifier
 
@@ -28,31 +29,37 @@ export default function Hooks(props) {
   localStorage.setItem('information', JSON.stringify(todos)); // On l'affichage dans le localstorage
 
 	// Équivalent à componentDidMount plus componentDidUpdate :
+	// On utilise ce Hook pour indiquer à React que notre composant doit exécuter quelque chose après chaque affichage
   useEffect(() => {
     // Mettre à jour le titre du document en utilisant l'API du navigateur, lorsqu'il y a un changement sur la variable
     document.title = `Vous avez cliqué ${count} fois`;
     localStorage.setItem('count', JSON.stringify(count));
+    // mettre cette condition ici eviter une répétition du alert 2 fois pour des raisons X
+    if (count == 5){
+      alert(count);
+    }
+
+    if (localStorage.getItem('count') == 2){
+      alert("oh oh");
+    }
+    console.log(count);
+    //console.log(Information());
   });
+
+  //setOldCount(count);
 
   // Capture de l'éventement lorsque je clique sur le bouton
   const handleCountChange = (event) => {
+    // ce deux console log ne fait pas le résultat escompté, le résultat sera affiché à la fin du useEffect
+    //console.log(count);
     setCount(count + 1);
+    //console.log(count);
   }
-
-
 
 	// Information est comme une fonction
   var Information = () => {
     return localStorage.getItem('count');
   };
-
-  // Je récupère l'information de la fonction
-  // Il y a une désynchronisation sur le nombre affiché
-  var compt = Information();
-
-  if (count == 5){
-    alert(count);
-  }
 
   return (
     <div>
